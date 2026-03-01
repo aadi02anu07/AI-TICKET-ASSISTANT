1565148f4a0f284139407a8f37a607f5f1511fe5
Aditya Srivastava <aadi02anu07@gmail.com>
2026-03-02 01:06:56 +0530

    Project started

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors" //acts as a middleware between front end and backend cause both are running at different ports/url
+
+const PORT = process.env.PORT || 3000
+const app = express()
+app.use(cors())
+app.use(express.json()) //so that we can accept json data
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected ✅");
+        app.listen(PORT, () => console.log("🚀 Server at http://localhost:3000"))
+    })
+    .cactch((err) => console.error("❌ MONGODB error:", err))
